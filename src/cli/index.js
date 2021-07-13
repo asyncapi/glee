01718131fd0a52d90bfb5e361dbcbc0270b398b1
Fran Mendez <fmvilas@gmail.com>
2021-07-13 14:11:04 +0200

    chore: add glee init

--- src/cli/index.js
+++ src/cli/index.js
@@ -1,14 +1,19 @@
 #!/usr/bin/env node --unhandled-rejections=strict
 
 import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
+import path, { dirname, resolve } from 'path'
 import nodemon from 'nodemon'
+import { accessSync, constants, mkdirSync, readFileSync, statSync, write, writeFileSync } from 'fs'
+import asyncapi from '@asyncapi/parser'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const args = process.argv.splice(2)
 
-if (args[0] === 'dev') {
+const command = args[0]
+const params = args.splice(1)
+
+if (command === 'dev') {
   nodemon({
     script: path.resolve(__dirname, 'start.js'),
     ext: '*',
@@ -22,8 +27,92 @@ if (args[0] === 'dev') {
   })
 
   nodemon.on('quit', process.exit)
-} else if (args[0] === 'start') {
+} else if (command === 'start') {
   import('./start.js')
+} else if (command === 'init') {
+  scaffoldProject(params[0])
 } else {
   console.error(`Unknown command "${args[0]}"`)
+}
+
+function scaffoldProject(name) {
+  const currentDir = process.cwd()
+  const projectPath = name ? resolve(currentDir, name) : currentDir
+  const functionsPath = resolve(projectPath, 'functions')
+  const asyncapiFilePath = resolve(projectPath, 'asyncapi.yaml')
+  const packageJsonPath = resolve(projectPath, 'package.json')
+  const gleePackageJson = JSON.parse(readFileSync(resolve(__dirname, '../../package.json')))
+
+  try {
+    mkdirSync(projectPath, { recursive: true })
+  } catch (e) {
+    // We did our best...
+  }
+
+  try {
+    mkdirSync(functionsPath, { recursive: true })
+  } catch (e) {
+    // We did our best...
+  }
+
+  let asyncapiFileExists = true
+  try {
+    statSync(asyncapiFilePath)
+  } catch (e) {
+    asyncapiFileExists = false
+  }
+
+  if (asyncapiFileExists) {
+    asyncapi.parse(readFileSync(asyncapiFilePath, { encoding: 'utf8' }))
+      .then(doc => {
+        const functionBody = `export default async function (event) {
+  // Your business logic here...
+}`
+        doc.channelNames().forEach(channelName => {
+          const channel = doc.channel(channelName)
+          if (channel.hasPublish() && channel.publish().json('operationId')) {
+            try {
+              writeFileSync(resolve(functionsPath, `${channel.publish().json('operationId')}.js`), functionBody, { mode: 'wx' })
+            } catch (e) {
+              // We did our best...
+            }
+          }
+        })
+      })
+      .catch(err => {
+        // If document is empty, simply ignore the error.
+        if (err.type !== 'https://github.com/asyncapi/parser-js/null-or-falsey-document') {
+          console.error(err)
+        }
+      })
+  } else {
+    const initialDoc = `asyncapi: 2.1.0
+info:
+  title: ${name || 'Hello, Glee!'}
+  version: 0.1.0
+servers:
+  mosquitto:
+    url: mqtt://test.mosquitto.org
+    protocol: mqtt
+channels: {}
+`
+    writeFileSync(asyncapiFilePath, initialDoc)
+  }
+
+  try {
+    writeFileSync(packageJsonPath, JSON.stringify({
+      name: name || 'hello-glee',
+      version: '0.1.0',
+      type: 'module',
+      scripts: {
+        dev: 'glee dev',
+        start: 'glee start'
+      },
+      dependencies: {
+        '@asyncapi/glee': gleePackageJson.version,
+      }
+    }, null, 2), { mode: 'wx' })
+  } catch (e) {
+    // We did our best...
+  }
 }
