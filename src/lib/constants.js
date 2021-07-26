429383fa4f05efd6202d757b54d24bcb33a5ec69
Jonas Lagoni <jonas-lt@live.dk>
2021-07-26 13:33:30 +0200

    Added missing check

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,3 +1,4 @@
+import { existsSync } from 'fs'
 import path from 'path'
 
 let GLEE_DIR
@@ -13,18 +14,21 @@ export async function setConstants(config) {
   GLEE_CONFIG_FILE_PATH = path.resolve(GLEE_DIR, 'glee.config.js')
   ASYNCAPI_FILE_PATH = path.resolve(GLEE_DIR, 'asyncapi.yaml')
 
-  //Load and overwrite configs from file
-  try {
-    let { default: projectConfigs } = await import(GLEE_CONFIG_FILE_PATH)
-    if (typeof projectConfigs === 'function') projectConfigs = await projectConfigs()
-
-    GLEE_DIR = projectConfigs.GLEE_DIR ? path.parse(projectConfigs.GLEE_DIR) : GLEE_DIR; 
-    GLEE_LIFECYCLE_DIR = projectConfigs.GLEE_LIFECYCLE_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_LIFECYCLE_DIR) : GLEE_LIFECYCLE_DIR; 
-    GLEE_FUNCTIONS_DIR = projectConfigs.GLEE_FUNCTIONS_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_FUNCTIONS_DIR) : GLEE_FUNCTIONS_DIR; 
-    ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? path.resolve(GLEE_DIR, projectConfigs.ASYNCAPI_FILE_PATH) : ASYNCAPI_FILE_PATH;
-  } catch (e) {
-    if (e.code !== 'ERR_MODULE_NOT_FOUND') {
-      return console.error(e)
+  if(existsSync(GLEE_CONFIG_FILE_PATH)){
+    //Load and overwrite configs from file
+    try {
+      let { default: projectConfigs } = await import(GLEE_CONFIG_FILE_PATH)
+      if (typeof projectConfigs === 'function') projectConfigs = await projectConfigs()
+      if(projectConfigs) {
+        GLEE_DIR = projectConfigs.GLEE_DIR ? path.parse(projectConfigs.GLEE_DIR) : GLEE_DIR; 
+        GLEE_LIFECYCLE_DIR = projectConfigs.GLEE_LIFECYCLE_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_LIFECYCLE_DIR) : GLEE_LIFECYCLE_DIR; 
+        GLEE_FUNCTIONS_DIR = projectConfigs.GLEE_FUNCTIONS_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_FUNCTIONS_DIR) : GLEE_FUNCTIONS_DIR; 
+        ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? path.resolve(GLEE_DIR, projectConfigs.ASYNCAPI_FILE_PATH) : ASYNCAPI_FILE_PATH;
+      }
+    } catch (e) {
+      if (e.code !== 'ERR_MODULE_NOT_FOUND') {
+        return console.error(e)
+      }
     }
   }
   
