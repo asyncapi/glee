59ea3d9d84661400461c784df0d321cab4b6c994
Jonas Lagoni <jonas-lt@live.dk>
2021-07-26 14:30:39 +0200

    Fixed lint

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -13,22 +13,22 @@ export async function setConstants(config) {
   GLEE_FUNCTIONS_DIR = path.resolve(GLEE_DIR, config.functionsDir || 'functions')
   GLEE_CONFIG_FILE_PATH = path.resolve(GLEE_DIR, 'glee.config.js')
   ASYNCAPI_FILE_PATH = path.resolve(GLEE_DIR, 'asyncapi.yaml')
-  await loadConfigsFromFile();
+  await loadConfigsFromFile()
   
   return getConstants()
 }
 
-async function loadConfigsFromFile(){
-  if(existsSync(GLEE_CONFIG_FILE_PATH)){
+async function loadConfigsFromFile() {
+  if (existsSync(GLEE_CONFIG_FILE_PATH)) {
     //Load and overwrite configs from file
     try {
       let { default: projectConfigs } = await import(GLEE_CONFIG_FILE_PATH)
       if (typeof projectConfigs === 'function') projectConfigs = await projectConfigs()
-      if(projectConfigs) {
-        GLEE_DIR = projectConfigs.GLEE_DIR ? path.parse(projectConfigs.GLEE_DIR) : GLEE_DIR; 
-        GLEE_LIFECYCLE_DIR = projectConfigs.GLEE_LIFECYCLE_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_LIFECYCLE_DIR) : GLEE_LIFECYCLE_DIR; 
-        GLEE_FUNCTIONS_DIR = projectConfigs.GLEE_FUNCTIONS_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_FUNCTIONS_DIR) : GLEE_FUNCTIONS_DIR; 
-        ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? path.resolve(GLEE_DIR, projectConfigs.ASYNCAPI_FILE_PATH) : ASYNCAPI_FILE_PATH;
+      if (projectConfigs) {
+        GLEE_DIR = projectConfigs.GLEE_DIR ? path.parse(projectConfigs.GLEE_DIR) : GLEE_DIR 
+        GLEE_LIFECYCLE_DIR = projectConfigs.GLEE_LIFECYCLE_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_LIFECYCLE_DIR) : GLEE_LIFECYCLE_DIR 
+        GLEE_FUNCTIONS_DIR = projectConfigs.GLEE_FUNCTIONS_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_FUNCTIONS_DIR) : GLEE_FUNCTIONS_DIR 
+        ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? path.resolve(GLEE_DIR, projectConfigs.ASYNCAPI_FILE_PATH) : ASYNCAPI_FILE_PATH
       }
     } catch (e) {
       if (e.code !== 'ERR_MODULE_NOT_FOUND') {
