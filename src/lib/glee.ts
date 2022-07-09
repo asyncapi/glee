630031d597d84b9ed8e8bfbb088107c0a580b4b2
sudoshreyansh <shreyanshjain14072002@gmail.com>
2022-07-09 18:07:53 +0530

    fixed linting error

--- src/lib/glee.ts
+++ src/lib/glee.ts
@@ -259,7 +259,7 @@ export default class Glee extends EventEmitter {
   }
 
   private _execErrorMiddleware (emws: ChannelErrorMiddlewareTuple[], index: number, error: Error, message: GleeMessage) {
-    emws[index].fn(error, message, (err: Error) => {
+    emws[index as number].fn(error, message, (err: Error) => {
       if (!emws[index+1]) return
       this._execErrorMiddleware.call(null, emws, index+1, err, message)
     })
