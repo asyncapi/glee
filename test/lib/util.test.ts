97775768bc26b7805dc694dca20597ec16267993
AceTheCreator <devaze007@gmail.com>
2022-07-15 12:41:20 +0100

    removed unused test

--- test/lib/util.test.ts
+++ test/lib/util.test.ts
@@ -1,6 +1,4 @@
 import 'jest-extended'
-import { tmpdir } from 'os'
-import {readFileSync, writeFileSync} from 'fs'
 import * as util from '../../src/lib/util.js'
 import GleeMessage from '../../src/lib/message.js'
 import Glee from '../../src/lib/glee.js'
@@ -49,29 +47,6 @@ describe('util', () => {
       expect(functionEvent.connection).toBeUndefined()
       expect(functionEvent.serverName).toEqual(message.serverName)
       expect(functionEvent.glee).toBe(glee)
-    })
-  })
-  describe('readGleeConfig', () => {
-    it('validates glee file path', () => {
-      const CONFIG_TEST_DATA = {
-        websocket: {
-          httpServer: 'customServer',
-          adapter: 'native', // Default. Can also be 'socket.io' or a reference to a custom adapter.
-        },
-        cluster: {
-          adapter: 'redis',
-          name: 'gleeCluster',
-          url: 'redis://localhost:6379',
-        },
-        generator: {
-          folder: 'output',
-          template: 'html-template',
-        },
-      }
-      const gleePath = tmpdir() + '/glee.config.js'
-      writeFileSync(gleePath, JSON.stringify(CONFIG_TEST_DATA), { encoding: 'ascii' })
-      const result = JSON.parse(readFileSync(gleePath, { encoding: 'ascii' }))
-      expect(result).toStrictEqual(CONFIG_TEST_DATA)
     })
   })
 })
