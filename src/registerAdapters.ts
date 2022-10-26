81391c87f7ddaade9a9e0db0968e5bac2a2d41cf
Ruchi <ruchipakhle@gmail.com>
2022-10-26 20:48:12 +0530

    chpick

--- src/registerAdapters.ts
+++ src/registerAdapters.ts
@@ -33,7 +33,7 @@ function registerAdapterForServer(serverName: string, server: Server, app: Glee,
       server,
       parsedAsyncAPI,
     })
-  } else if (['kafka'].includes(protocol)) {
+  } else if (['kafka', 'kafka-secure'].includes(protocol)) {
     app.addAdapter(KafkaAdapter, {
       serverName,
       server,
