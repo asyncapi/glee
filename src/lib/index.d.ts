a0fb54f2442c1f730317826ada9bc83a5b96a020
souvik <souvikde.ns@gmail.com>
2022-11-15 18:01:56 +0530

    refactor: fix lint issues

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,22 +1,22 @@
-import GleeAdapter from "./adapter.js"
-import GleeClusterAdapter from "./cluster.js"
-import GleeConnection from "./connection.js"
-import Glee from "./glee.js"
+import GleeAdapter from './adapter.js'
+import GleeClusterAdapter from './cluster.js'
+import GleeConnection from './connection.js'
+import Glee from './glee.js'
 
-type WebSocketServerType = "native" | "socket.io";
+type WebSocketServerType = 'native' | 'socket.io'
 
 export type GleeClusterAdapterConfig = {
-  adapter?: string | typeof GleeClusterAdapter;
-  name?: string;
-  url: string;
-};
+  adapter?: string | typeof GleeClusterAdapter,
+  name?: string,
+  url: string,
+}
 
 export type WebsocketAdapterConfig = {
   server?: {
-    httpServer?: any
-    adapter?: WebSocketServerType | typeof GleeAdapter
-    port?: number
-  }
+    httpServer?: any,
+    adapter?: WebSocketServerType | typeof GleeAdapter,
+    port?: number,
+  },
   client?: {
     authentication?: {
       http: string
@@ -27,42 +27,42 @@ export type WebsocketAdapterConfig = {
 export type MqttAdapterConfig = {
   authentication?: {
     cert?: string
-    userPassword?: {username: string, password: string}
-    clientId?: string
+    userPassword?: { username: string; password: string },
+    clientId?: string,
   }
 }
 
 export type GleeConfig = {
-  websocket?: WebsocketAdapterConfig
-  cluster?: GleeClusterAdapterConfig;
-  mqtt?: MqttAdapterConfig
-};
+  websocket?: WebsocketAdapterConfig,
+  cluster?: GleeClusterAdapterConfig,
+  mqtt?: MqttAdapterConfig,
+}
 
 export type GleeFunctionReturn = {
-  send?: GleeFunctionReturnSend[];
-  reply?: GleeFunctionReturnReply[];
-  broadcast?: GleeFunctionReturnBroadcast[];
-};
+  send?: GleeFunctionReturnSend[],
+  reply?: GleeFunctionReturnReply[],
+  broadcast?: GleeFunctionReturnBroadcast[]
+}
 
 export type GleeFunctionEvent = {
-  glee: Glee;
-  serverName: string;
-  connection?: GleeConnection;
-  payload?: any;
-  headers?: { [key: string]: string };
-  channel?: string;
-};
+  glee: Glee,
+  serverName: string,
+  connection?: GleeConnection,
+  payload?: any,
+  headers?: { [key: string]: string },
+  channel?: string
+}
 
 export type GleeFunctionReturnSend = {
-  payload?: any;
-  headers?: { [key: string]: string };
-  channel?: string;
-  server?: string;
-};
+  payload?: any,
+  headers?: { [key: string]: string },
+  channel?: string,
+  server?: string,
+}
 
-export type GleeFunctionReturnReply = GleeFunctionReturnSend;
-export type GleeFunctionReturnBroadcast = GleeFunctionReturnSend;
+export type GleeFunctionReturnReply = GleeFunctionReturnSend
+export type GleeFunctionReturnBroadcast = GleeFunctionReturnSend
 
 export type GleeFunction = (
   event: GleeFunctionEvent
-) => Promise<GleeFunctionReturn>;
+) => Promise<GleeFunctionReturn>
