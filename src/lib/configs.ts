0395f55fb9b946f9c6849c7344fd528aff5fd889
Khuda Dad Nomani <khudadad414@gmail.com>
2022-12-20 11:57:03 +0000

    error handling

--- src/lib/configs.ts
+++ src/lib/configs.ts
@@ -1,6 +1,7 @@
 import { existsSync } from 'fs'
 import path from 'path'
 import { pathToFileURL } from 'url'
+import { logErrorLine } from './logger.js'
 
 interface Config {
   functionsDir?: string,
@@ -41,7 +42,10 @@ async function loadConfigsFromFile() {
     GLEE_DIR = projectConfigs.GLEE_DIR || GLEE_DIR 
     GLEE_LIFECYCLE_DIR = projectConfigs.GLEE_LIFECYCLE_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_LIFECYCLE_DIR) : GLEE_LIFECYCLE_DIR 
     GLEE_FUNCTIONS_DIR = projectConfigs.GLEE_FUNCTIONS_DIR ? path.resolve(GLEE_DIR, projectConfigs.GLEE_FUNCTIONS_DIR) : GLEE_FUNCTIONS_DIR 
-    ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? path.resolve(GLEE_DIR, projectConfigs.ASYNCAPI_FILE_PATH) : ASYNCAPI_FILE_PATH
+    ASYNCAPI_FILE_PATH = projectConfigs.ASYNCAPI_FILE_PATH ? projectConfigs.ASYNCAPI_FILE_PATH : ASYNCAPI_FILE_PATH
+    if(!ASYNCAPI_FILE_PATH){
+      logErrorLine("Unable to load the AsyncAPI file. please set its path in your glee config file. ()")
+    }
     return projectConfigs
   } catch (e) {
     if (e.code !== 'ERR_MODULE_NOT_FOUND') {
@@ -55,11 +59,9 @@ export function findSpecFile(baseDir): string{
   const foundFiles = files.filter(file => existsSync(path.resolve(baseDir, file)))
   
   if (foundFiles.length === 1) {
-    return path.resolve(baseDir, files[indexOfFoundFile])
-  } else if (foundFiles.length === 0) {
-    throw new Error('AsyncAPI file was not found at the root of your project. you can set its path in config file.')
-  } else {
-    throw new Error('Multiple AsyncAPI files found at the root of your project. Please make sure to choose one in your config file.')
+    return path.resolve(baseDir, foundFiles[0])
+  }  else {
+    return undefined
   }
 }
 export function getConfigs(): { [key: string]: string } {
