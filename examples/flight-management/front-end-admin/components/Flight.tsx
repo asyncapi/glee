4f42ca590b293a7d9956921106df949b9a4223b1
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2023-01-10 17:54:45 +0000

    chore: add flight management example (#369)

---
+++ examples/flight-management/front-end-admin/components/Flight.tsx
@@ -0,0 +1,87 @@
+'use client'
+import React, { useState } from 'react'
+
+export default function Flight({ _flight, sendJsonMessage }: any) {
+  const [flight, setFlight] = useState(_flight)
+
+  const {
+    flight_no,
+    arrival,
+    departure,
+    destin_terminal,
+    origin_terminal,
+    origin_city,
+    destin_city,
+    status,
+  } = flight
+
+  const updateField = (event: any, field: string) => {
+    const updatedFlight = {
+      ...flight,
+      [field]: event.target.value,
+    }
+    console.log('flight updated.')
+    setFlight(updatedFlight)
+  }
+  const publishData = () => {
+    sendJsonMessage({ name: 'updateFlight', updatedFlight: flight })
+  }
+  return (
+    <>
+      <td className='text-yellow-500'>{flight_no}</td>
+      <td>
+        <input
+          type='datetime-local'
+          value={getDateString(departure)}
+          onChange={(e) => updateField(e, 'departure')}
+        />
+      </td>
+      <td>
+        <input
+          type='datetime-local'
+          value={getDateString(arrival)}
+          onChange={(e) => updateField(e, 'arrival')}
+        />
+      </td>
+      <td className=' text-yellow-400'>{origin_city}</td>
+      <td className=' text-yellow-400'>{destin_city}</td>
+      <td>
+        <select value={status} onInput={(e) => updateField(e, 'status')}>
+          <option>ON TIME</option>
+          <option>DELAYED</option>
+          <option>CANCELED</option>
+        </select>
+      </td>
+      <td>
+        <input
+          value={origin_terminal}
+          onInput={(e) => updateField(e, 'origin_terminal')}
+        />
+      </td>
+      <td>
+        <input
+          value={destin_terminal}
+          onInput={(e) => updateField(e, 'destin_terminal')}
+        />
+      </td>
+      <td>
+        <button onClick={publishData} className='bg-accent px-2'>
+          Submit
+        </button>
+      </td>
+    </>
+  )
+}
+
+const getDateString = (date: string) => {
+  const d = new Date(date)
+  return d.getFullYear() +
+    '-' +
+    ('0' + (d.getMonth() + 1)).slice(-2) +
+    '-' +
+    ('0' + d.getDate()).slice(-2) +
+    'T' +
+    ('0' + d.getHours()).slice(-2) +
+    ':' +
+    ('0' + d.getMinutes()).slice(-2)
+}
