f3233c9bb96da29431db90f024680fd44a88844c
Ritik Rawal <ritikrawal@MacBook-Pro-85.local>
2023-02-02 22:15:50 +0530

    updated client code for http server

---
+++ examples/anime-http/client/functions/upcomingAnimeListController.ts
@@ -0,0 +1,14 @@
+import path from 'path'
+import fs from 'fs'
+
+export default async function (event) {
+    const payload = event.payload
+    const dbPath = path.resolve('./db.json')
+    const read = () => JSON.parse(fs.readFileSync(dbPath, 'utf-8'))
+    const write = (data) => { fs.writeFileSync(dbPath, data, { encoding: 'utf-8' }) }
+    let db = read()
+    write(JSON.stringify([...db, payload]))
+    return {
+        send: []
+    }
+}
