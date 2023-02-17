a5ca06db87b59ab09edb6892399ba720e75e9221
Ritik Rawal <ritikrawal@MacBook-Pro-85.local>
2023-02-17 19:40:48 +0530

    fixed code smell

--- src/adapters/http/client.ts
+++ src/adapters/http/client.ts
@@ -13,27 +13,26 @@ class HttpClientAdapter extends Adapter {
 
   async _connect(): Promise<this> {
     const headers = {}
-      const config: HttpAdapterConfig = await this.resolveProtocolConfig('http')
-      const clientConfig = config?.client
-      headers['Authenticaton'] = clientConfig?.authentication?.token
+    const config: HttpAdapterConfig = await this.resolveProtocolConfig("http")
+    const clientConfig = config?.client
+    headers["Authenticaton"] = clientConfig?.authentication?.token
     return this
   }
   async _send(message: GleeMessage): Promise<void> {
-
     const method = message.payload.method //get post put
     const url = `http://localhost:${message.payload.port}/${message.serverName}`
     const body = message.payload.body
     const query = message.payload.query
 
     try {
-      const response = await axios({
+      await axios({
         method,
         url,
         data: body,
         params: query,
       })
     } catch (err) {
-      this.emit('error', err)
+      this.emit("error", err)
     }
   }
 }
