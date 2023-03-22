2557652c72d47e9128bdae893d8305285aad6509
Ritik Rawal <ritikpr307@gmail.com>
2023-03-22 12:08:32 +0530

    feat: add http adapter support for glee (#320)
    Co-authored-by: Ritik Rawal <ritikrawal@MacBook-Pro-5.local>%0ACo-authored-by: Ritik Rawal <ritikrawal@MacBook-Pro-85.local>%0ACo-authored-by: Ritik Rawal <ritikrawal@ip-192-168-0-101.ap-south-1.compute.internal>%0ACo-authored-by: Ritik Rawal <ritikrawal@ip-192-168-0-102.ap-south-1.compute.internal>

---
+++ src/adapters/http/client.ts
@@ -0,0 +1,63 @@
+import Adapter from '../../lib/adapter.js'
+import GleeMessage from '../../lib/message.js'
+import got from 'got'
+import { HttpAuthConfig, HttpAdapterConfig } from '../../lib/index.js'
+import http from 'http'
+class HttpClientAdapter extends Adapter {
+
+  name(): string {
+    return 'HTTP client'
+  }
+  async connect(): Promise<this> {
+    this.emit('connect', {
+      name: this.name(),
+      adapter: this,
+      connection: http,
+      channel: this.channelNames,
+    })
+    return this
+  }
+
+  async send(message: GleeMessage): Promise<void> {
+    const headers = {}
+    const config: HttpAdapterConfig = await this.resolveProtocolConfig('http')
+    const auth: HttpAuthConfig = await this.getAuthConfig(config.client.auth)
+    headers['Authentication'] = auth?.token
+    const serverUrl = this.serverUrlExpanded
+    this.channelNames.forEach(async (channelName) => {
+      const channelInfo = this.parsedAsyncAPI.channel(channelName)
+      const httpChannelBinding = channelInfo.binding('http')
+      const channelServers = channelInfo.servers()
+      const isChannelServers = !channelServers.length || channelServers.includes(message.serverName)
+      if (
+        httpChannelBinding && isChannelServers
+      ) {
+        const method = httpChannelBinding.method
+        const url = `${serverUrl}/${channelName}`
+        const body:any = message.payload
+        const query: { [key: string]: string } | { [key: string]: string[] } = message.query
+        got({
+          method,
+          url,
+          json: body,
+          searchParams: JSON.parse(JSON.stringify(query)),
+        })
+          .then((res) => {
+            const msg = this.createMessage(channelName, res.body)
+            this.emit('message', msg, http)
+          })
+          .catch((err) => {
+            this.emit('error', err)
+          })
+      }
+    })
+  }
+  private createMessage(channelName: string, payload: any) {
+    return new GleeMessage({
+      payload: JSON.parse(JSON.stringify(payload)),
+      channel: channelName,
+    })
+  }
+}
+
+export default HttpClientAdapter
