948e8f1a6c94e2ef4989ca7e6d86a7fed641e64c
Kaushik Rishi <rishi.cp01@gmail.com>
2023-05-27 23:37:14 +0530

    feat: add better logging for mqtt

--- src/adapters/mqtt/index.ts
+++ src/adapters/mqtt/index.ts
@@ -3,6 +3,7 @@ import Adapter from '../../lib/adapter.js'
 import GleeMessage from '../../lib/message.js'
 import { MqttAuthConfig, MqttAdapterConfig } from '../../lib/index.js'
 import { SecurityScheme } from '@asyncapi/parser';
+import { logLineWithIcon } from '../../lib/logger.js';
 
 interface IMQTTHeaders {
   cmd?: string;
@@ -52,14 +53,14 @@ class MqttAdapter extends Adapter {
     const X509SecurityReq = securityRequirements.find(
       (sec) => sec.type() === 'X509'
     )
-    
+
     return {
       userAndPasswordSecurityReq,
       X509SecurityReq
     }
 
   }
-  
+
   private async initializeClient(data: ClientData) {
 
     const {
@@ -137,6 +138,19 @@ class MqttAdapter extends Adapter {
       const binding = operation.binding('mqtt')
       this.client.subscribe(channel, {
         qos: binding?.qos ? binding.qos : 0,
+      }, (err, granted) => {
+        if (err) {
+          logLineWithIcon('x', `Error while trying to subscribe to \`${channel}\` topic.`, {
+            highlightedWords: [channel],
+            iconColor: '#f00',
+            disableEmojis: true,
+          })
+          console.log(err.message)
+          return
+        }
+        logLineWithIcon(':zap:', `Subscribed to \`${channel}\` topic.`, {
+          highlightedWords: [channel],
+        })
       })
     })
   }
