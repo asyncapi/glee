d20798dc9635bef298dd205b876da223bab60d11
Alexander Godwin <alecgee73@gmail.com>
2023-07-01 14:11:47 +0100

    authentication

--- examples/anime-http/client/auth/trendingAnime.js
+++ examples/anime-http/client/auth/trendingAnime.js
@@ -1,22 +1,6 @@
-// /* eslint-disable no-undef */
+/* eslint-disable no-undef */
 
-// //@ts-ignore
-// import { Message } from "@asyncapi/glee";
-// import axios from "axios";
-
-// export async function serverAuth({ headers, callback: done }) {
-//   await axios.get("https://jsonplaceholder.typicode.com/todos/1");
-
-//   console.log("running in auth file in auth folder");
-
-//   console.log("headers");
-
-//   // console.log("network data", res.data)
-
-//   // callback(false, 401, "Unauthorized");
-//   //   console.log(typeof done);
-//   done(false);
-// }
+// @ts-ignore
 
 export async function clientAuth({ parsedAsyncAPI, serverName }) {
   console.log("serverName", serverName)
@@ -25,8 +9,3 @@ export async function clientAuth({ parsedAsyncAPI, serverName }) {
     token: process.env.TOKEN,
   }
 }
-
-// export const lifecycleEvent = "onAuth";
-
-// //specify auth that's being run
-// export const security = ["tokens"];
