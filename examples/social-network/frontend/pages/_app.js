ab8ddba894e703c8c33c000824316286c724b688
Khuda Dad Nomani <khudadad414@gmail.com>
2023-08-21 09:51:57 +0100

    formatting

--- examples/social-network/frontend/pages/_app.js
+++ examples/social-network/frontend/pages/_app.js
@@ -1,5 +1,7 @@
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps} />
+  )
 }
