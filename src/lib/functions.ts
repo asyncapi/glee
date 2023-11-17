4b296c1baa36cf730fd77c9597bdb6580f47b035
Khuda Dad Nomani <khudadad414@gmail.com>
2023-11-17 14:07:40 +0000

    fix stash

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -171,7 +171,7 @@ export async function trigger({
 
     functionResult?.reply?.forEach((reply) => {
       const replyMessages = createReplies(reply, message, parsedAsyncAPI)
-      if (replyMessages || replyMessages.length < 1) {
+      if (!(replyMessages && replyMessages.length > 0)) {
         return
       }
       replyMessages.forEach(replyMessage => {
