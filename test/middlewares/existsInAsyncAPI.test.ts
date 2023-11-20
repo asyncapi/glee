6512162ba83231b845f4382da0bcbae44f2e3b77
Khuda Dad Nomani <32505158+KhudaDad414@users.noreply.github.com>
2023-11-20 09:21:32 +0000

    feat: show an error when no operation is defined (#568)

--- test/middlewares/existsInAsyncAPI.test.ts
+++ test/middlewares/existsInAsyncAPI.test.ts
@@ -1,35 +1,52 @@
 import 'jest-extended'
-import {AsyncAPIDocumentV2 as AsyncAPIDocument} from '@asyncapi/parser'
+import { AsyncAPIDocumentInterface, Parser } from '@asyncapi/parser'
 import existsInAsyncAPI from '../../src/middlewares/existsInAsyncAPI.js'
 import GleeMessage from '../../src/lib/message.js'
 
-const TEST_ASYNCAPI_DOCUMENT = new AsyncAPIDocument({
-  asyncapi: '2.2.0',
-  info: {title: '', version: ''},
-  servers: {
-    test: {
-      url: 'mqtt://fake-url',
-      protocol: 'mqtt',
-    }
-  },
-  channels: {
-    'test/channel': {
-      publish: {
-        message: {
-          payload: {
-            type: 'string',
+describe('existsInAsyncAPI', () => {
+
+
+  const parser = new Parser()
+  let document: AsyncAPIDocumentInterface
+  beforeAll(async () => {
+    const parsedResult = await parser.parse({
+      asyncapi: "3.0.0",
+      info: {
+        title: "",
+        version: ""
+      },
+      servers: {
+        test: {
+          host: "fake-url",
+          protocol: "mqtt"
+        }
+      },
+      channels: {
+        testChannel: {
+          address: "test/channel"
+        },
+        noOperationChannel: {
+          address: "no/operation"
+        }
+      },
+      operations: {
+        receiveTest: {
+          action: "receive",
+          channel: {
+            "$ref": "#/channels/testChannel"
           }
         }
       }
-    }
-  }
-})
+    })
 
-describe('existsInAsyncAPI', () => {
+    if (parsedResult.document)
+      document = parsedResult.document
+  })
+
   it('checks if channel exists', done => {
-    const middleware = existsInAsyncAPI(TEST_ASYNCAPI_DOCUMENT);
+    const middleware = existsInAsyncAPI(document);
     const message = new GleeMessage({
-      channel: 'test/channel'
+      channel: 'testChannel'
     });
 
     middleware(message, err => {
@@ -39,9 +56,9 @@ describe('existsInAsyncAPI', () => {
   })
 
   it('error if channel does not exist', done => {
-    const middleware = existsInAsyncAPI(TEST_ASYNCAPI_DOCUMENT);
+    const middleware = existsInAsyncAPI(document);
     const message = new GleeMessage({
-      channel: 'test/channel2'
+      channel: 'nonExistentChannel'
     });
 
     middleware(message, err => {
@@ -49,4 +66,48 @@ describe('existsInAsyncAPI', () => {
       done()
     })
   })
+
+  it('error if no send operation for outbound message', done => {
+    const middleware = existsInAsyncAPI(document);
+    const message = {
+      channel: 'noOperationChannel',
+      isInbound: () => false,
+      isOutbound: () => true
+    } as GleeMessage
+
+    middleware(message, err => {
+      expect(err).toBeInstanceOf(Error);
+      expect(err?.message).toMatch(/No 'send' operation defined/);
+      done();
+    });
+  });
+
+  it('error if no receive operation for inbound message', done => {
+    const middleware = existsInAsyncAPI(document);
+    const message = {
+      channel: 'noOperationChannel',
+      isInbound: () => true,
+      isOutbound: () => false
+    } as GleeMessage
+
+    middleware(message, err => {
+      expect(err).toBeInstanceOf(Error);
+      expect(err?.message).toMatch(/No 'receive' operation defined/);
+      done();
+    });
+  });
+
+  it('passes if channel has required operations', done => {
+    const middleware = existsInAsyncAPI(document);
+    const message = {
+      channel: 'testChannel',
+      isOutbound: () => false,
+      isInbound: () => true
+    } as GleeMessage
+
+    middleware(message, err => {
+      expect(err).toBeUndefined();
+      done();
+    });
+  });
 })
