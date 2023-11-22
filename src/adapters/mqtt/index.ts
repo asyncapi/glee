9bc2de045e8f1b288ffac19c9d8da8ab23a249db
souvik <souvikde.ns@gmail.com>
2023-11-22 20:12:16 +0530

    fix: use channel.address instead of id (#582)

--- src/adapters/mqtt/index.ts
+++ src/adapters/mqtt/index.ts
@@ -45,13 +45,14 @@ class MqttAdapter extends Adapter {
   }
 
   private getSecurityReqs() {
-
     let userAndPasswordSecurityReq
     let X509SecurityReq
 
-    const securityRequirements = this.AsyncAPIServer.security().map(e => e.all().map(e => e.scheme()))
+    const securityRequirements = this.AsyncAPIServer.security().map((e) =>
+      e.all().map((e) => e.scheme())
+    )
 
-    securityRequirements.forEach(security => {
+    securityRequirements.forEach((security) => {
       for (const sec of security) {
         const securityType = sec.type().toLocaleLowerCase()
         switch (securityType) {
@@ -62,14 +63,23 @@ class MqttAdapter extends Adapter {
             X509SecurityReq = sec
             break
           default:
-            this.emit("error", new Error(`Invalid security type '${securityType}' specified for server '${this.serverName}'. Please double-check your configuration to ensure you're using a supported security type. Here is a list of supported types: ${Object.values(SecurityTypes)}`))
+            this.emit(
+              'error',
+              new Error(
+                `Invalid security type '${securityType}' specified for server '${
+                  this.serverName
+                }'. Please double-check your configuration to ensure you're using a supported security type. Here is a list of supported types: ${Object.values(
+                  SecurityTypes
+                )}`
+              )
+            )
         }
       }
     })
 
     return {
       userAndPasswordSecurityReq,
-      X509SecurityReq
+      X509SecurityReq,
     }
   }
 
@@ -110,7 +120,9 @@ class MqttAdapter extends Adapter {
     this.client.on('close', () => {
       this.emit('close', {
         connection: this.client,
-        channels: this.channelNames,
+        channels: this.channelNames.map((channelName) =>
+          this.parsedAsyncAPI.channels().get(channelName).address()
+        ),
       })
     })
 
@@ -139,23 +151,40 @@ class MqttAdapter extends Adapter {
 
   private subscribe(channels: string[]) {
     channels.forEach((channel) => {
-      const binding = this.parsedAsyncAPI.channels().get(channel).bindings().get('mqtt')?.value()
-      this.client.subscribe(channel, {
-        qos: binding?.qos ? binding.qos : 0,
-      }, (err, granted) => {
-        if (err) {
-          logLineWithIcon('x', `Error while trying to subscribe to \`${channel}\` topic.`, {
-            highlightedWords: [channel],
-            iconColor: '#f00',
-            disableEmojis: true,
-          })
-          console.log(err.message)
-          return
+      const binding = this.parsedAsyncAPI
+        .channels()
+        .get(channel)
+        .bindings()
+        .get('mqtt')
+        ?.value()
+      this.client.subscribe(
+        channel,
+        {
+          qos: binding?.qos ? binding.qos : 0,
+        },
+        (err, granted) => {
+          if (err) {
+            logLineWithIcon(
+              'x',
+              `Error while trying to subscribe to \`${channel}\` topic.`,
+              {
+                highlightedWords: [channel],
+                iconColor: '#f00',
+                disableEmojis: true,
+              }
+            )
+            console.log(err.message)
+            return
+          }
+          logLineWithIcon(
+            ':zap:',
+            `Subscribed to \`${channel}\` topic with QoS ${granted?.[0].qos}`,
+            {
+              highlightedWords: [channel],
+            }
+          )
         }
-        logLineWithIcon(':zap:', `Subscribed to \`${channel}\` topic with QoS ${granted?.[0].qos}`, {
-          highlightedWords: [channel],
-        })
-      })
+      )
     })
   }
 
@@ -216,7 +245,12 @@ class MqttAdapter extends Adapter {
 
   _send(message: GleeMessage): Promise<void> {
     return new Promise((resolve, reject) => {
-      const binding = this.parsedAsyncAPI.channels().get(message.channel).bindings().get('mqtt')?.value()
+      const binding = this.parsedAsyncAPI
+        .channels()
+        .get(message.channel)
+        .bindings()
+        .get('mqtt')
+        ?.value()
       this.client.publish(
         message.channel,
         message.payload,
@@ -244,11 +278,11 @@ class MqttAdapter extends Adapter {
       dup: packet.dup,
       length: packet.length,
     }
-
+    const id = this.parsedAsyncAPI.channels().filter(channel => channel.address() === packet.topic)[0].id()
     return new GleeMessage({
       payload: packet.payload,
       headers,
-      channel: packet.topic,
+      channel: id,
     })
   }
 
