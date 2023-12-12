5780a9635c29de43aee8637bf605f7bfd199e2d1
souvik <souvikde.ns@gmail.com>
2023-12-12 08:12:51 +0530

    feat: constructing url in auth

--- src/adapters/ws/client.ts
+++ src/adapters/ws/client.ts
@@ -51,11 +51,6 @@ class WsClientAdapter extends Adapter {
         const modedAuth = await gleeAuth.processClientAuth({ url, headers, query: {} })
         headers = modedAuth.headers
         url = modedAuth.url
-        if (modedAuth.query) {
-          Object.keys(modedAuth.query).forEach(k => {
-            url.searchParams.set(k, modedAuth.query[k])
-          })
-        }
       }
       this.clients.push({
         channel: channelName,
